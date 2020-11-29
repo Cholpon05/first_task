351ab441380c31e708c8a09a6667c357729c5857
Cholpon05 <70993279+Cholpon05@users.noreply.github.com>
2020-11-29 11:08:02 +0600

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,7 @@
+name = input("Enter your name")
+month = input("Enter month of your studies")
+if month.lower() == "1":
+    print("You are beginner")
+else:
+    if month.lower()!= "5":
+        print("You are intermediate")
